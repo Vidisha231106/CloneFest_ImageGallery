da05be02be9287d8ff908e9cae7c3ad28e1344a1
Vidisha Dewan <vidishadewan2020@gmail.com>
2025-09-13 14:26:24 +0530

    correction for deployment again

--- gallery-backend/src/index.js
+++ gallery-backend/src/index.js
@@ -11,13 +11,14 @@ import albumRoutes from './routes/albumRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import tagRoutes from './routes/tagRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 // --- Middleware ---
-app.use(cors({
-  origin: 'https://clonefestimagegalleryfrontend-production.up.railway.app' // Your frontend URL
-}));
+const frontendURL = process.env.FRONTEND_URL || 'http://localhost:3000';
+
 app.use(express.json({ limit: '50mb' })); // Increase limit for base64 images
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
